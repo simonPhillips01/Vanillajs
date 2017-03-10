60dca7e44ec4feb7be8e19f8f6bf207b7a07fb04
Simon Phillips <simon.phillips01@gmail.com>
2017-03-10 15:32:23 +0000

    Amendments

---
+++ js-without-jQuery/js/main.js
@@ -0,0 +1,26 @@
+var newItemCounter = 1;
+var ourList = document.querySelector("#our-list");
+var ourButton = document.getElementById("our-button");
+var ourHeadline = document.getElementById("our-headline");
+var listItems = document.querySelectorAll("#our-list li");
+
+ourList.addEventListener("click", activateItem);
+
+function activateItem(e) {
+  if (e.target.nodeName == "LI") {
+    ourHeadline.innerHTML = e.target.innerHTML;
+
+    for (i = 0; i < e.target.parentNode.children.length; i++) {
+      e.target.parentNode.children[i].classList.remove("active");
+    }
+
+    e.target.classList.add("active");
+  }
+}
+
+ourButton.addEventListener("click", createNewItem);
+
+function createNewItem() {
+  ourList.innerHTML += "<li>Something new " + newItemCounter + "</li>";
+  newItemCounter ++;
+}
